15420ef8c3d5c8a4df0f1ce07db48ee9b56c44e9
ljsomm <miscrits.lucas@gmail.com>
2023-09-18 22:31:12 -0300

    feat: add new seo items

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import {Head, NextScript, Main, Html} from "next/document";
+const Document = () => {
+  return(
+    <Html>
+      <Head>
+        <link rel="canonical" href="https://lucasjsdev.com/" />
+        <meta name="description" content="Site developed by Lucas Juan Souza de Oliviera Miranda Moraes, Software Engineer."></meta>
+      </Head>
+      <body>
+        <Main/>
+        <NextScript/>
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
