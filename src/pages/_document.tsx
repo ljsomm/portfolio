183a675d482fccc3b97b24b611a7da34137f5e58
ljsomm <miscrits.lucas@gmail.com>
2023-09-28 16:39:02 -0300

    feat: seo improve tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,12 @@ const Document = () => {
   return (
     <Html>
       <Head>
-        <link rel="canonical" href="https://lucasjsdev.com/" />
+        <link rel="canonical" href="https://www.lucasjsdev.com/" />
         <meta
           name="description"
           content="Site developed by Lucas Juan Souza de Oliviera Miranda Moraes, Software Engineer."
         ></meta>
+        <meta name="robots" content="index,follow"></meta>
       </Head>
       <body>
         <Main />
